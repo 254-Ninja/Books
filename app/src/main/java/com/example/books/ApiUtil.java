9427c0bf7c775aa75cf5a033a7a995468befa843
perfect-code-ninja <muthomidenis62@gmail.com>
2019-08-16 12:37:26 +0300

    apiutil

--- app/src/main/java/com/example/books/ApiUtil.java
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -1,13 +1,35 @@
 package com.example.books;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class ApiUtil {
     private ApiUtil(){}
 
-    public static final String BASe_API_URI =
+    public static final String BASE_API_URI =
             "https://www.googleapis.com/books/v1/volumes";
     public  static URL buildUrl(String title){
-        String fullUrl = BASe_API_URI + "?q="
+        String fullUrl = BASE_API_URI + "?q=" + title;
+        URL url = null;
+        try {
+            url = new URL(fullUrl);
+        }
+        catch (Exception e) {
+
+        }
+        return url;
+        }
+
+    public static String getJson(URL url) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream stream = connection.getInputStream();
+        Scanner scanner = new Scanner(stream);
+        scanner.useDelimiter("\\A");
     }
-}
+    }
+
+
