7a6be300aec85ada52611c759a6e1317192fc36c
perfect-code-ninja <muthomidenis62@gmail.com>
2019-08-17 13:18:21 +0300

    entry points

--- app/src/main/java/Book.java
+++ app/src/main/java/Book.java
@@ -1,2 +1,11 @@
 public class Book {
+    public String id;
+    public String title;
+    public String subTitle;
+    public String[] authors;
+    public String publishers;
+    public String publisherData;
+
+    
+
 }
