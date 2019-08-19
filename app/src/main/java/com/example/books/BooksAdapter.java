02692a9ac1b7c6694656fde44f7bcbfa1db1c714
perfect-code-ninja <muthomidenis62@gmail.com>
2019-08-19 14:28:29 +0300

    holders

--- app/src/main/java/com/example/books/BooksAdapter.java
+++ app/src/main/java/com/example/books/BooksAdapter.java
@@ -1,4 +1,27 @@
 package com.example.books;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
 public class BooksAdapter {
+
+    public class BookViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvtAuthors;
+        TextView tvDate;
+        TextView tvPublisher;
+
+        public BookViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            tvtAuthors = (TextView) itemView.findViewById(R.id.tvAuthors);
+            tvDate = (TextView) itemView.findViewById(R.id.tvPublishedDate);
+            tvPublisher = (TextView)itemView.findViewById(R.id.tvPublisher);
+
+
+        }
+    }
 }
