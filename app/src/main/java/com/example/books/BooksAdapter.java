a32cb7638628701eb90d52008c11bd7d2180c86a
perfect-code-ninja <muthomidenis62@gmail.com>
2019-08-20 21:18:28 +0300

    final commit

--- app/src/main/java/com/example/books/BooksAdapter.java
+++ app/src/main/java/com/example/books/BooksAdapter.java
@@ -1,20 +1,21 @@
 package com.example.books;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHolder> {
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHolder>  {
 
     ArrayList<Book> books;
-    public BooksAdapter(ArrayList<Book> books){
+    public BooksAdapter(ArrayList<Book> books) {
         this.books = books;
     }
     @Override
@@ -22,7 +23,11 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHold
         Context context = parent.getContext();
         View itemView = LayoutInflater.from(context)
                 .inflate(R.layout.book_list_item, parent, false);
+
+
         return new BookViewHolder(itemView);
+
+
     }
 
     @Override
@@ -37,36 +42,50 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHold
         return books.size();
     }
 
-    public class BookViewHolder extends RecyclerView.ViewHolder{
+
+    public class BookViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tvTitle;
-        TextView tvtAuthors;
+        TextView tvAuthors;
         TextView tvDate;
         TextView tvPublisher;
 
         public BookViewHolder(View itemView) {
             super(itemView);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
-            tvtAuthors = (TextView) itemView.findViewById(R.id.tvAuthors);
+            tvAuthors= (TextView) itemView.findViewById(R.id.tvAuthors);
             tvDate = (TextView) itemView.findViewById(R.id.tvPublishedDate);
-            tvPublisher = (TextView)itemView.findViewById(R.id.tvPublisher);
+            tvPublisher =(TextView) itemView.findViewById(R.id.tvPublisher);
+            itemView.setOnClickListener(this);
 
         }
-        public  void bind (Book book) {
+        public void bind (Book book) {
             tvTitle.setText(book.title);
             String authors="";
             int i=0;
-            for (String author:book.authors){
+            /*for (String author:book.authors.get()) {
                 authors+=author;
                 i++;
-                if(i<book.authors.length){
+                if(i<book.authors.get().length) {
                     authors+=", ";
                 }
-            }
-            tvtAuthors.setText(authors);
-            tvDate.setText(book.publisherData);
-            tvPublisher.setText(book.publishers);
+            }*/
 
+            tvAuthors.setText(book.authors.get());
+            tvDate.setText(book.publishedDate.get());
+            tvPublisher.setText(book.publisher.get());
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Log.d("Click", String.valueOf(position));
+            //gets the book from the arrayList
+            Book selectedBook = books.get(position);
+            Intent intent = new Intent(view.getContext(), BookDetail.class);
+            intent.putExtra("Book", selectedBook);
+            view.getContext().startActivity(intent);
         }
     }
 }
